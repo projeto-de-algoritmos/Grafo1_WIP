72479d802ed77e23c90f20f856503e5bd030ccb5
devsalula <saleh.nazih.dev@gmail.com>
2021-02-16 18:25:43 -0300

    Search Links with the Crawler
    Co-authored-by: Calebe Mendes <calebersmendes@gmail.com>

--- src/index.js
+++ src/index.js
@@ -11,11 +11,25 @@ const getGameSiteHtml = async (url) => {
         }
     });
 }
+const getFirstGames = async (url) => {
+    const htmlFound = await getGameSiteHtml(url);
+    const gamesJson = parser.getListOfGames(htmlFound);
+    return gamesJson;
+}
 
 async function main() {
-    const htmlFound = await getGameSiteHtml('https://store.steampowered.com/app/1293160/The_Medium/');
-    const gamesJson = parser.getListOfGames(htmlFound);
-    console.log(gamesJson);
+    let firstJsonGames = await getFirstGames('https://store.steampowered.com/app/1293160/The_Medium/');
+    for(let key in firstJsonGames) {
+        var countKey = Object.keys(firstJsonGames).length;
+        if(countKey < 100) {
+            const url = firstJsonGames[key]['link'];
+            console.log(url);
+            const htmlFound = await getGameSiteHtml(url);
+            const gamesJson = parser.getListOfGames(htmlFound);
+            firstJsonGames = Object.assign(firstJsonGames, gamesJson);
+        }
+    }
+    console.log(firstJsonGames);
 }
 
 main();
