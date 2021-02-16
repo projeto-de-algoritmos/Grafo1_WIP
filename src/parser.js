59dd1633a378810f9157b2af24b38ea42e244d2c
devsalula <saleh.nazih.dev@gmail.com>
2021-02-16 17:38:59 -0300

    Parse Game Json
    Co-authored-by: Calebe Mendes <calebersmendes@gmail.com>

--- src/parser.js
+++ src/parser.js
@@ -2,24 +2,42 @@ const cheerio = require('cheerio');
 
 const regex = /\{"rgApps":{.*}/;
 
-const buildLinks = (html) => {
+const getGamePrice = (html) => {
     const $ = cheerio.load(html);
-    let stringJson; 
-    $("script").map((i, script) => {
+    return $('div.discount_final_price').first().text();
+}
+
+const getGameJson = (html) => {
+    const $ = cheerio.load(html);
+    let json; 
+    $("script").map((_, script) => {
         script.children.map(child => {
-            const str = child.data;
-            const response = regex.exec(str);
-            if (response != null) {
-                stringJson = JSON.parse(response[0]);
+            const regexResponse = regex.exec(child.data);
+            if (regexResponse != null) {
+                json = JSON.parse(regexResponse[0]);
             }
         });
     });
-    return stringJson;
+    return json;
+}
+
+const parseGameJson = (jsonGames) => {
+    let games = jsonGames['rgApps'];
+    for (let key in games) {
+        let gameName = games[key].name.replace(/[^a-zA-Z ]/g,'');
+        gameName = gameName.replace(/\s/g, '_');
+        let link = 'https://store.steampowered.com/app/' + key + '/' + gameName;
+        games[key]['link'] = link;
+        games[key]['discount_block'] = getGamePrice(games[key]['discount_block']);
+        delete games[key]['descids']
+    }
+    return games;
 }
 
 const getLinksHtml = (html) => {
-    const links = buildLinks(html);
-    console.log(links);
+    const jsonGames = getGameJson(html);
+    const jsonParsed = parseGameJson(jsonGames);
+    console.log(jsonParsed);
 }
 
 module.exports = { getLinksHtml };
